79867facb6825b0955fb75c3afbd8ff8096db58f
d0as8 <d0as8@aaanet.ru>
2015-09-14 14:28:52 +0300

    fix

--- riotbus.js
+++ riotbus.js
@@ -1,15 +1,14 @@
 ﻿var RiotBusMixin = {
     init: function() {
-        this.bus.tags.push(this);
+        this.bus.tags.push(this)
     },
-
     bus: {
         tags: [],
-
         trigger: function(event, params) {
-            this.tags.forEach(function(tag) {
-                tag.trigger(event, params);
-            })
+            var i, length = this.tags.length;
+            for (i = 0; i < length; i++) {
+                this.tags[i].trigger(event, params);
+            }
         }
     }
-}
+};
